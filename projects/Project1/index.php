d5bf59316275aa8f77ae3b401a7a3adc613a8dd4
Joshua S <Joshua_Saun@protonmail.com>
2024-02-02 16:12:31 -0600

    Dev: Added sample home page to sample project

--- projects/Project1/index.php
+++ projects/Project1/index.php
@@ -4,43 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-    <title>Projects</title>
+    <title>Project 1</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <div class="container-fluid">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-
-
-
-
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="../about.php">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Projects</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../contact.php">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/sandbox.php">Sandbox</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <h1>Projects Page</h1>
-    <ul>
-        <li>
-            <a href="/projects/project1/">Project One</a>
-        </li>
-    </ul>
+    <h1>Project 1</h1>
 </body>
 
 </html>
