4a0187f4e4ba6a2b5ba7973420663654d305c45d
Joshua S <Joshua_Saun@protonmail.com>
2024-02-02 16:02:42 -0600

    Dev: Caontact page added w/ boilerplate

---
+++ contact.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact me</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="about.php">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./projects/">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="sandbox.php">Sandbox</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <h1>Contact Page</h1>
+</body>
+
+</html>
